cc1d0f60a94bc89894703a84a3ae862c30c7a025
Mukunthan A R <mukunthan.1255@gmail.com>
2026-05-25 08:27:46 +0530

    useProject pool db

--- models/useProject.js
+++ models/useProject.js
@@ -1,4 +1,4 @@
-const { connectDB } = require("../db/db");
+const { connectDB, pool } = require("../db/db");
 
 const handleError = (err) => ({
   status: 500,
@@ -8,12 +8,10 @@ const handleError = (err) => ({
 
 // Fetch projects created by a specific user
 const getProjectsByCreator = async (userId) => {
-  const client = await connectDB();
-
   const query = "SELECT * FROM projects WHERE created = $1 ORDER BY start_date";
 
   try {
-    const result = await client.query(query, [userId]);
+    const result = await pool.query(query, [userId]);
 
     if (result.rows.length > 0) {
       return {
@@ -31,8 +29,6 @@ const getProjectsByCreator = async (userId) => {
   } catch (err) {
     console.error("Error in getProjectsByCreator:", err);
     return handleError(err);
-  } finally {
-    client.release();
   }
 };
 
