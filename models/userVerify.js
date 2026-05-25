dfd21ff5ab3982a88a00966cebe6540af84346b1
Mukunthan A R <mukunthan.1255@gmail.com>
2026-05-25 08:32:22 +0530

    userVerify pool db

--- models/userVerify.js
+++ models/userVerify.js
@@ -1,17 +1,16 @@
-const { connectDB } = require("../db/db");
+const { connectDB, pool } = require("../db/db");
 
 // ✅ Update user activation status to true
 const verifyUserById = async (userId) => {
-  const client = await connectDB();
   const query = `
-      UPDATE users
-      SET is_activated = TRUE
-      WHERE user_id = $1
-      RETURNING user_id, name, email, company, role, is_activated
-    `;
+    UPDATE users
+    SET is_activated = TRUE
+    WHERE user_id = $1
+    RETURNING user_id, name, email, company, role, is_activated
+  `;
 
   try {
-    const res = await client.query(query, [userId]);
+    const res = await pool.query(query, [userId]);
 
     if (res.rows.length === 0) {
       return { success: false, status: 404, message: "User not found" };
@@ -20,8 +19,6 @@ const verifyUserById = async (userId) => {
     return { success: true, status: 200, data: res.rows[0] };
   } catch (err) {
     return { message: err.message, status: 500, success: false };
-  } finally {
-    client.release();
   }
 };
 
